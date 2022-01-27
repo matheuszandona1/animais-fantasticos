81123914a4f9b2dbbfbe897607042a6aef4aa409
matheus <mastronelli88@gmail.com>
2022-01-27 14:13:22 -0300

    adicionando dropdown-menu e outsideClick function

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import initAccordion from './modules/accordion.js'
 import initTabNav from './modules/tabnav.js'
 import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
+import initDropdownMenu from './modules/dropdown-menu.js'
 
 
 initScrollSuave();
@@ -12,3 +13,5 @@ initAccordion();
 initTabNav();
 initModal();
 initTooltip();
+initDropdownMenu();
+outsideClick();
