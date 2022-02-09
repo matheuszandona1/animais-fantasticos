4f8e6862d6dc5c3eaaffb0f9e9404324af9cf762
matheus <mastronelli88@gmail.com>
2022-02-09 11:52:01 -0300

    adicionando fetch e criando uma api ⚙️

--- js/script.js
+++ js/script.js
@@ -6,8 +6,11 @@ import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
 import initDropdownMenu from './modules/dropdown-menu.js'
 import initMenuMobile from './modules/menu-mobile.js'
-import initAnimaNumeros from './modules/anima-numeros.js'
 import initFuncionamento from './modules/funcionamento.js'
+import initFetchAnimais from './modules/fetch-animais.js'
+import initFetchBtc from './modules/fetch-btc.js'
+
+
 
 
 initScrollSuave();
@@ -18,5 +21,6 @@ initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
+initFetchAnimais();
+initFetchBtc();
