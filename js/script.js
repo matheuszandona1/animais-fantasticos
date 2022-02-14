cabf2c52dd8f40a4839b25bfa57fcbbf04aa963d
matheus <mastronelli88@gmail.com>
2022-02-14 10:54:59 -0300

    transformando accordion em classe

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 import ScrollSuave from './modules/scroll-suave.js';
-import initAnimacaoScroll from './modules/animacao-scroll.js';
-import initAccordion from './modules/accordion.js'
+import Accordion from './modules/accordion.js'
 import initTabNav from './modules/tabnav.js'
 import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
@@ -9,12 +8,18 @@ import initMenuMobile from './modules/menu-mobile.js'
 import initFuncionamento from './modules/funcionamento.js'
 import initFetchAnimais from './modules/fetch-animais.js'
 import initFetchBtc from './modules/fetch-btc.js'
+import initAnimacaoScroll from './modules/animacao-scroll.js';
+
+
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
-initAnimacaoScroll();
-initAccordion();
+const accordion = new Accordion('[data-anime="accordion"] dt')
+accordion.init();
+
+
+
 initTabNav();
 initModal();
 initTooltip();
@@ -23,4 +28,5 @@ initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBtc();
+initAnimacaoScroll();
 
